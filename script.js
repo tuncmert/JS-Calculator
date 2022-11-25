7f1d5f4c07e381272673181a56d28f8e003588f5
tuncmert <merttunc20@gmail.com>
2022-11-25 14:30:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const display = document.querySelector('.calculator-input');
+const keys = document.querySelector('.calculator-keys');
+
+let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay() {
+    display.value = displayValue;
+}
+
+keys.addEventListener('click', function (e) {
+    const element = e.target;
+
+    if (!element.matches('button')) return;
+
+    switch(element.value){
+        case"+":
+        case"-":
+        case"*":
+        case"/":
+        case"=":
+            handleOperator(element.value);
+            break;
+        case'.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            inputNumber(element.value);
+    }
+    updateDisplay();
+
+    // if (element.classList.contains('operator')) {
+    //     handleOperator(element.value);
+    //     updateDisplay();
+    //     return;
+    // };
+
+    // if (element.classList.contains('decimal')) {
+    //     inputDecimal();
+    //     updateDisplay();
+    //     return;
+    // };
+
+    // if (element.classList.contains('clear')) {
+    //     clear();
+    //     updateDisplay();
+    //     return;
+    // };
+    
+    // inputNumber(element.value);
+    // updateDisplay();
+});
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+    if (operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    };
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator)
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    };
+    waitingForSecondValue = true;
+    operator = nextOperator;
+};
+
+function calculate(first, second, op) {
+    if (op === '+') {
+        return first + second;
+    } else if (op === '-') {
+        return first - second;
+    } else if (op === '*') {
+        return first * second;
+    } else if (op === '/') {
+        return first / second;
+    }
+    return second;
+}
+
+function inputNumber(num) {
+    if (waitingForSecondValue) {
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+        displayValue = displayValue === '0' ? num : displayValue + num;
+    }
+
+};
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.';
+    };
+};
+
+function clear() {
+    displayValue = '0';
+    firstValue=null;
+    waitingForSecondValue=false;
+}
